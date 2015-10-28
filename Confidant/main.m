d7bbfa53e9f4fc4a4f85e9d915cfc827ab4a1fe5
Eava.wu <wuyihua86@gmail.com>
2015-10-28 22:23:09 +0800

    Initial Commit

---
+++ Confidant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Confidant
+//
+//  Created by Eavawu on 10/28/15.
+//  Copyright © 2015 Eavawu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
